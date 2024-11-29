3cf93204847501fb2415bd7a853e6725c5cc6421
Salah <salah.abdullah.art@gmail.com>
2024-11-29 12:25:06 +0100

    update navigation drawer

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -2,24 +2,15 @@
   <v-card>
     <v-layout>
       <v-navigation-drawer
-        v-model="drawer"
-        :rail="rail"
-        permanent
-        @click="rail = false"
+        expand-on-hover
+        rail
       >
-        <v-list-item
-          prepend-icon="mdi-home-city"
-          title="Lava"
-          nav
-        >
-          <template #append>
-            <v-btn
-              icon="mdi-chevron-left"
-              variant="text"
-              @click.stop="rail = !rail"
-            />
-          </template>
-        </v-list-item>
+        <v-list>
+          <v-list-item
+            prepend-icon="mdi-account"
+            title="account"
+          />
+        </v-list>
 
         <v-divider />
 
@@ -28,36 +19,28 @@
           nav
         >
           <v-list-item
-            prepend-icon="mdi-home-city"
-            title="Home"
-            value="home"
+            prepend-icon="mdi-folder"
+            title="My Files"
+            value="myfiles"
           />
 
           <v-list-item
-            prepend-icon="mdi-account"
-            title="My Account"
-            value="account"
+            prepend-icon="mdi-account-multiple"
+            title="Shared with me"
+            value="shared"
           />
 
           <v-list-item
-            prepend-icon="mdi-account-group-outline"
-            title="Users"
-            value="users"
+            prepend-icon="mdi-star"
+            title="Starred"
+            value="starred"
           />
-
         </v-list>
       </v-navigation-drawer>
-      <v-main style="height: 250px" />
+
+      <v-main style="height:100em" />
     </v-layout>
   </v-card>
 </template>
-<script>
-export default {
-  data () {
-    return {
-      drawer: true,
-      rail: true,
-    }
-  },
-}
+<script setup lang="ts">
 </script>
