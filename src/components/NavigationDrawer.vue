2179d5e747980cca3c72c625673f2891b6585b4a
Salah <salah.abdullah.art@gmail.com>
2024-11-29 11:59:03 +0100

    init project
    create navigation drawer

---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        v-model="drawer"
+        :rail="rail"
+        permanent
+        @click="rail = false"
+      >
+        <v-list-item
+          prepend-icon="mdi-home-city"
+          title="Lava"
+          nav
+        >
+          <template #append>
+            <v-btn
+              icon="mdi-chevron-left"
+              variant="text"
+              @click.stop="rail = !rail"
+            />
+          </template>
+        </v-list-item>
+
+        <v-divider />
+
+        <v-list
+          density="compact"
+          nav
+        >
+          <v-list-item
+            prepend-icon="mdi-home-city"
+            title="Home"
+            value="home"
+          />
+
+          <v-list-item
+            prepend-icon="mdi-account"
+            title="My Account"
+            value="account"
+          />
+
+          <v-list-item
+            prepend-icon="mdi-account-group-outline"
+            title="Users"
+            value="users"
+          />
+
+        </v-list>
+      </v-navigation-drawer>
+      <v-main style="height: 250px" />
+    </v-layout>
+  </v-card>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      drawer: true,
+      rail: true,
+    }
+  },
+}
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <navigation-drawer />
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
